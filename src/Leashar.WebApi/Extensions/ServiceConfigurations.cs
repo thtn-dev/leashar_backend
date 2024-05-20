c74f939f3e062ef92bd4500476b721f982367c59
thtn-dev <thtn.1611.dev@gmail.com>
2024-05-20 09:41:37 +0700

    refactor config service and pipelines

---
+++ src/Leashar.WebApi/Extensions/ServiceConfigurations.cs
@@ -0,0 +1,15 @@
+﻿namespace Leashar.WebApi.Extensions
+{
+    public static class ServiceConfigurations
+    {
+        public static WebApplicationBuilder AddServices(this WebApplicationBuilder builder)
+        {
+            builder.Services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            return builder;
+        }
+    }
+}
--- src/Leashar.WebApi/Program.cs
+++ src/Leashar.WebApi/Program.cs
@@ -1,34 +1,17 @@
 
+using Leashar.WebApi.Extensions;
+
 namespace Leashar.WebApi
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            var builder = WebApplication.CreateBuilder(args);
+            var builder = WebApplication.CreateBuilder(args)
+                .AddServices();
 
-            // Add services to the container.
-
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
-
-            var app = builder.Build();
-
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
-
-            //app.UseHttpsRedirection();
-
-            app.UseAuthorization();
-
-
-            app.MapControllers();
+            var app = builder.Build()
+                .ConfigurePipelines();
 
             app.Run();
         }
